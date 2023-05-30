8d928fc835321402f512ff4d674b63791df6f2b1
xulio <despairnatural@gmail.com>
2023-05-30 15:45:46 -0300

    Variavel ambiente api

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:10101/v2",
-  baseURL: "https://api.prod.aprincipalbb.com.br/api/v2",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export { api };
